32ed9bd9254e22e762836d01848a63dd0c9131ab
sanozie <sanoziec@gmail.com>
2025-12-02 14:02:28 -0800

    Init

---
+++ api/src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono'
+import { start } from 'workflow/api'
+import { handleUserSignup } from './workflows/job.js'
+
+const app = new Hono();
+
+app.post("/api/signup", async (c) => {
+  const { email } = await c.req.json();
+  await start(handleUserSignup, [email]);
+  return c.json({ message: "User signup workflow started" });
+});
+export default app;
